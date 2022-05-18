a0be85b166b649832785c540c1c74f53e89d611a
Shahad Alfayez <89224138+ShahadAlfayez88@users.noreply.github.com>
2022-05-18 17:06:01 +0300

    Create ShahadAlfayezHW1.dart

---
+++ ShahadAlfayezHW1.dart
@@ -0,0 +1,11 @@
+main(){
+const name="Shahad Alfayez "; // this is a const THAT CANNOT CHANGE
+  var date="2000-05-09"; // this the date of birth
+  var phone="05553499959"; // this is variable phone number
+
+  print('Hello ${name}your information is the following:\n your birth date is ${date} \n and your phone number ${phone}');
+
+  print('the forth index is ${phone[4]}');
+
+  print('your name length is ${name.length}' );
+}
